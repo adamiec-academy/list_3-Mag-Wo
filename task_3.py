83b36642f019f38534d0d0a0e508a8d99ea8bb6b
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-01 18:34:18 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,14 @@
-# Remove this comment to confirm that this task is done
+
+from turtle import *
+from random import randint
+
+speed("fast")
+
+step = 30
+mount = [0, 50, 130, 210, 170, 280, 220, 150, 80, 50, 20, 10, 150, 190, 265, 200, 280, 120, 80, 150, 0]
+
+for i in range(len(mount)):
+    goto(i*step, mount[i] + randint(-25,25))
+goto(0,0)
+
+exitonclick()
