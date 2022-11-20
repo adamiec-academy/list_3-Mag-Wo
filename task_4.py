27d87e33ed7fbd032646c3c5eb86fa920efc8f4f
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-20 13:31:40 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,11 @@
 def tower(n):
-    pass
+    total_width = 2*n-1
 
+    for i in range (1,n+1):
+        current_width = 2*i - 1
+
+        for _ in range(3):
+            print(" "* (n-i) + "#"*(i+(i-1)) + " "*(n-i))
 
 def towers(data):
     pass
